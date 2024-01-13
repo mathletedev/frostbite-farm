2157d47c70b30fe835caa1d00b325a21292d8d8b
Neal Wang <nealwang.sh@protonmail.com>
2024-01-12 23:35:18 -0800

    yes

---
+++ Scenes/watering_can.gd
@@ -0,0 +1,10 @@
+extends StaticBody2D
+
+@onready var collider: CollisionShape2D = $CollisionShape2D
+
+func _ready():
+	colli
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
