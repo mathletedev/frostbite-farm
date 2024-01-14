2cb92076b3b47b3701b73a7a59cf581734b3dd38
Neal Wang <nealwang.sh@protonmail.com>
2024-01-13 23:40:56 -0800

    revert changes

--- Scripts/seeds.gd
+++ Scripts/seeds.gd
@@ -2,24 +2,38 @@ extends "item.gd"
 
 @export var plant_scene: PackedScene
 @export var count: int = 3
-@export var plant_offset: int = 32
+@export var plant_offset: int = 48
+@export_flags_2d_physics var mask: int = 4
 
-@onready var text: RichTextLabel = $RichTextLabel
+@onready var text_label: RichTextLabel = $RichTextLabel
 @onready var sprite: Sprite2D = $Sprite2D
 
 func _custom_ready() -> void:
-	text.text = str(count)
+	text_label.text = str(count)
 
 func _custom_process(_delta):
-	if picked_up && Input.is_action_just_released("interact") && GameManager.can_place == true:
+	if picked_up && Input.is_action_just_released("interact"):
+		var point: Vector2 = Vector2.ZERO
+		point.x = (floor(player.position.x / 32)) * 32 + 16
+		point.y = (floor((player.position.y + plant_offset) / 32)) * 32 - 16
+
+		var parameters := PhysicsPointQueryParameters2D.new()
+		parameters.position = point
+		parameters.collide_with_areas = true
+		parameters.collide_with_bodies = true
+		parameters.collision_mask = mask
+
+		var collisions := get_world_2d().direct_space_state.intersect_point(parameters)
+		if collisions.size() > 0:
+			return
+
 		count -= 1
 
 		var plant: Node2D = plant_scene.instantiate()
-		plant.position.x = (int(player.position.x) / 32) * 32 + 16
-		plant.position.y = (int(player.position.y + plant_offset) / 32) * 32 - 16
+		plant.position = point
 		get_tree().root.add_child(plant)
 
-		text.text = str(count)
+		text_label.text = str(count)
 
 		if count == 0:
 			queue_free()
