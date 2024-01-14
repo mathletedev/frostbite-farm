9c4dc40c6f79ebd5a50e4e7fe479ebbf43d54e8b
Neal Wang <nealwang.sh@protonmail.com>
2024-01-14 12:33:57 -0800

    yes

--- Scripts/fire_lamp.gd
+++ Scripts/fire_lamp.gd
@@ -8,22 +8,45 @@ extends "item.gd"
 @onready var light: PointLight2D = $PointLight2D
 @onready var timer: Timer = $Timer
 @onready var bar: TextureProgressBar = $TextureProgressBar
-@onready var lifetime_countdown: int = lifetime
+@onready var heat_collider: CollisionShape2D = $Heat/CollisionShape2D
+@onready var fire: AnimatedSprite2D = $AnimatedSprite2D
+@onready var warning_scene: PackedScene = preload("res://Scenes/warning.tscn")
+
+var lifetime_countdown: int = 0
 
 func get_type() -> String:
 	return "fire_lamp"
 
 func _custom_ready() -> void:
+	bar.value = 0
 	timer.timeout.connect(_on_timer)
+	fire.visible = false
+	heat_collider.disabled = true
 
 func _draw() -> void:
+	if lifetime_countdown <= 0:
+		return
+
 	var colour = warmth_colour
 	colour.a = (sin(Time.get_ticks_msec() * pulse_rate) + 1) / 4 + 0.25
 	draw_circle_arc(Vector2.ZERO, warmth_radius, 0, 360, colour)
 
 func _custom_process(_delta: float) -> void:
 	queue_redraw()
-	light.energy = (sin(Time.get_ticks_msec() * pulse_rate) + 1) / 4 + 0.5
+
+	light.energy = (sin(Time.get_ticks_msec() * pulse_rate) + 1) / 4 + 0.5 if lifetime_countdown > 0 else 0.0
+
+	if touching_player && Input.is_action_just_released("interact"):
+		bar.value = 100
+		lifetime_countdown = lifetime
+		fire.visible = true
+		heat_collider.disabled = false
+
+		var warning := get_node_or_null("Warning")
+		if warning != null:
+			warning.queue_free()
+
+		timer.start()
 
 func draw_circle_arc(center, radius, angle_from, angle_to, color):
 	var nb_points = 32
@@ -37,4 +60,21 @@ func draw_circle_arc(center, radius, angle_from, angle_to, color):
 		draw_line(points_arc[index_point], points_arc[index_point + 1], color, 1)
 
 func _on_timer() -> void:
+	if lifetime_countdown <= 0:
+		return
+
 	lifetime_countdown -= 1
+	bar.value = (float(lifetime_countdown) / lifetime) * 100
+
+	if lifetime_countdown == 3:
+		var warning: Node2D = warning_scene.instantiate()
+		warning.position.y = -20
+		add_child(warning)
+
+	if lifetime_countdown == 0:
+		fire.visible = false
+		heat_collider.disabled = true
+
+		var warning := get_node_or_null("Warning")
+		if warning != null:
+			warning.queue_free()
