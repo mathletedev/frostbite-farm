0a0882ecd2639547fdef9e672191dbced0661495
Peter Liu <peter9342@gmail.com>
2024-01-14 11:36:03 -0800

    shop ui

--- Scripts/shopkeeper.gd
+++ Scripts/shopkeeper.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var InteractButton: Sprite2D = get_node("Interact Button")
+@onready var ShopUI: CanvasLayer = get_node("Shop UI")
 var Player = null
 var inShopRange = false
 
@@ -12,11 +13,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("interact(shop)") and inShopRange:
-		print("shop opened")
-	
+		ShopUI.visible = true
 	pass
 
-
+#entered interaction radius of shopkeeper
 func _on_interaction_radius_body_entered(body):
 	if body.name == "Player":
 		InteractButton.visible = true
@@ -24,9 +24,15 @@ func _on_interaction_radius_body_entered(body):
 		inShopRange = true
 	pass 
 
-
+#exit interaction radius of shopkeeper
 func _on_interaction_radius_body_exited(body):
 	if body.name == "Player":
 		InteractButton.visible = false
 		inShopRange = false
+		ShopUI.visible = false
+	pass 
+
+# closes shop ui
+func _on_button_pressed():
+	ShopUI.visible = false
 	pass 
