465a59738f75269480850f3a42ab7d795c1fd696
Peter Liu <peter9342@gmail.com>
2024-01-13 22:31:43 -0800

    shopkeeper

--- Scripts/shopkeeper.gd
+++ Scripts/shopkeeper.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@onready var InteractButton: Sprite2D = get_node("Interact Button")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,10 +9,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("f"):
+		print("shop open")
 	pass
 
 
 func _on_interaction_radius_body_entered(body):
 	if body.name == "Player":
-		pass
+		InteractButton.visible = true
+	pass # Replace with function body.
+
+
+func _on_interaction_radius_body_exited(body):
+	if body.name == "Player":
+		InteractButton.visible = false
 	pass # Replace with function body.
