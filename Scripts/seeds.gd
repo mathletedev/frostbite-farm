e6ec56d80432da509aafc41174308544fa0466c2
Peter Liu <peter9342@gmail.com>
2024-01-13 23:29:56 -0800

    Revert "Merge branch 'main' of https://github.com/mathletedev/frostbite-farm"
    This reverts commit 17cccf7475d9e76fba69253caa5e0cda4a1f2fd8, reversing
    changes made to 934612dbc8c237fca1ffb2e911e2ca6bf034fe4e.

--- Scripts/seeds.gd
+++ Scripts/seeds.gd
@@ -2,8 +2,7 @@ extends "item.gd"
 
 @export var plant_scene: PackedScene
 @export var count: int = 3
-@export var plant_offset: int = 48
-@export_flags_2d_physics var mask: int = 4
+@export var plant_offset: int = 32
 
 @onready var text: RichTextLabel = $RichTextLabel
 @onready var sprite: Sprite2D = $Sprite2D
@@ -12,25 +11,12 @@ func _custom_ready() -> void:
 	text.text = format(count)
 
 func _custom_process(_delta):
-	if picked_up && Input.is_action_just_released("interact"):
-		var point: Vector2 = Vector2.ZERO
-		point.x = (floor(player.position.x / 32)) * 32 + 16
-		point.y = (floor((player.position.y + plant_offset) / 32)) * 32 - 16
-
-		var parameters := PhysicsPointQueryParameters2D.new()
-		parameters.position = point
-		parameters.collide_with_areas = true
-		parameters.collide_with_bodies = true
-		parameters.collision_mask = mask
-
-		var collisions := get_world_2d().direct_space_state.intersect_point(parameters)
-		if collisions.size() > 0:
-			return
-
+	if picked_up && Input.is_action_just_released("interact") && GameManager.can_place == true:
 		count -= 1
 
 		var plant: Node2D = plant_scene.instantiate()
-		plant.position = point
+		plant.position.x = (int(player.position.x) / 32) * 32 + 16
+		plant.position.y = (int(player.position.y + plant_offset) / 32) * 32 - 16
 		get_tree().root.add_child(plant)
 
 		text.text = format(count)
