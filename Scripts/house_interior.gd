6da36349e23e0a484431c6bf1c971c6369f00043
Neal Wang <nealwang.sh@protonmail.com>
2024-01-14 13:21:35 -0800

    yes

--- Scripts/house_interior.gd
+++ Scripts/house_interior.gd
@@ -1,30 +1,15 @@
 extends Node
 
-@onready var InteractHouse: Sprite2D = $HouseInterior
+@onready var sprite: Sprite2D = $HouseInterior
 @onready var house_collider: CollisionPolygon2D = $StaticBody2D/CollisionPolygon2D
-@onready var tile_map_collider: TileMap = get_node("%TileMap")
-
-var Player = null
-var inHouseRange = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var tile_map: TileMap = get_node("%TileMap")
 
 func _on_interaction_radius_body_entered(body):
 	if body.name == "Player":
-		InteractHouse.visible = true
-		tile_map_collider.set_layer_enabled(0,false)
-		tile_map_collider.set_layer_enabled(1,false)
-		Player = body
-		inHouseRange = true
-	pass 
-
+		sprite.modulate.a = 0.5
+		tile_map.set_layer_enabled(0,false)
+		tile_map.set_layer_enabled(1,false)
 
 func _on_interaction_radius_body_exited(body):
-	print("player exited")
 	if body.name == "Player":
-		InteractHouse.visible = false
-		inHouseRange = false
-		tile_map_collider.set_layer_enabled(0,true)
-	pass # Replace with function body.
+		sprite.modulate.a = 1
