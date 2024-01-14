c741965d48a29a76b7658391e4bbb55541f98628
Peter Liu <peter9342@gmail.com>
2024-01-14 15:48:53 -0800

    revamped movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,59 +1,39 @@
 extends CharacterBody2D
 
-@export var max_speed: float = 150
-@export var acceleration: float = 300
-@export var friction: float = 1000
-@export var dash_speed: float = 300
-@export var dash_time: float = 0.15
-@export var dash_wait: float = 1
+@export var speed = 125
+@export var friction:float = 50
+
+@export var dash_speed: float = 200.0
+@export var dash_time: float = .45
+@export var dash_cooldown: float = .15
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var dash_timer: Timer = $DashTimer
-@onready var dash_waiter: Timer = $DashWait
-
-@onready var curr_max_speed: float = max_speed
+@onready var dash_cooldown_timer: Timer = $DashCooldown
 
 var can_dash = true
+var dash_velocity = Vector2(0,0)
+var input_direction = Vector2(0,0)
+var in_dash = false
 
 func _ready() -> void:
-	dash_timer.wait_time = dash_time
-	dash_timer.timeout.connect(_on_dash_timer)
-	dash_waiter.wait_time = dash_wait
-	dash_waiter.timeout.connect(_on_dash_waiter)
+	pass
 
-func _process(_delta: float) -> void:
-	var input := get_input()
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = dash_velocity + input_direction.normalized() * speed
 
-	if Input.is_action_pressed("dash") && input != Vector2.ZERO:
-		dash(input)
-
-func _physics_process(delta: float) -> void:
-	var input := get_input()
-
-	move(delta, input)
-	animate()
-
-func get_input() -> Vector2:
-	var input := Vector2.ZERO
-	input.x = Input.get_axis("left", "right")
-	input.y = Input.get_axis("up", "down")
-
-	return input
-
-func move(delta: float, input: Vector2) -> void:
-	if curr_max_speed != dash_speed:
-		if input == Vector2.ZERO:
-			if velocity.length() > friction * delta:
-				velocity -= velocity.normalized() * friction * delta
-			else:
-				velocity = Vector2.ZERO
-		else:
-			velocity += input * acceleration * delta
-
-	velocity = velocity.limit_length(curr_max_speed)
-
+	return input_direction
+	
+func _process(delta):
+	if in_dash == false: 
+		input_direction = get_input()
+	if Input.is_action_pressed("dash") && input_direction != Vector2.ZERO:
+		dash(input_direction, delta)
+	
 	move_and_slide()
-
+	animate()
+	
 func animate() -> void:
 	if velocity.length() < 1:
 		sprite.animation = "idle"
@@ -66,20 +46,20 @@ func animate() -> void:
 		sprite.flip_h = 1
 	elif velocity.x > 0:
 		sprite.flip_h = 0
-
-func dash(input: Vector2) -> void:
+		
+func dash(input_direction: Vector2, delta) -> void:
 	if !can_dash:
 		return
 	can_dash = false
-
-	velocity = input.normalized() * dash_speed
-	curr_max_speed = dash_speed
-
+	in_dash = true
+	
+	velocity = dash_speed * input_direction
+	
 	dash_timer.start()
-	dash_waiter.start()
-
-func _on_dash_timer() -> void:
-	curr_max_speed = max_speed
-
-func _on_dash_waiter() -> void:
-	can_dash = true
+	dash_cooldown_timer.start()
+	
+func _on_dash_timer_timeout():
+	in_dash = false
+	
+func _on_dash_cooldown_timeout():
+	can_dash = true	
