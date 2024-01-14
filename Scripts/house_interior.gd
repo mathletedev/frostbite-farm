a9f5223b0efcd3193dce56471b0f78065185bb9f
Neal Wang <nealwang.sh@protonmail.com>
2024-01-14 13:29:31 -0800

    yes

--- Scripts/house_interior.gd
+++ Scripts/house_interior.gd
@@ -1,15 +1,17 @@
 extends Node
 
-@onready var sprite: Sprite2D = $HouseInterior
+@onready var sprite: Sprite2D = $Sprite2D
 @onready var house_collider: CollisionPolygon2D = $StaticBody2D/CollisionPolygon2D
 @onready var tile_map: TileMap = get_node("%TileMap")
 
 func _on_interaction_radius_body_entered(body):
 	if body.name == "Player":
 		sprite.modulate.a = 0.5
-		tile_map.set_layer_enabled(0,false)
-		tile_map.set_layer_enabled(1,false)
+		tile_map.tile_set.set_physics_layer_collision_mask(0, 0)
+		tile_map.tile_set.set_physics_layer_collision_layer(0, 0)
 
 func _on_interaction_radius_body_exited(body):
 	if body.name == "Player":
 		sprite.modulate.a = 1
+		tile_map.tile_set.set_physics_layer_collision_mask(0, 5)
+		tile_map.tile_set.set_physics_layer_collision_layer(0, 5)
