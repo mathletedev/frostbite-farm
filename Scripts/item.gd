b8f58837aef30427d07faeb0e7da22d3f0af41f0
Neal Wang <nealwang.sh@protonmail.com>
2024-01-13 19:56:07 -0800

    refix stuff

--- Scripts/item.gd
+++ Scripts/item.gd
@@ -13,6 +13,9 @@ var player: Node2D
 func get_type() -> String:
 	return ""
 
+func _custom_ready() -> void:
+	pass
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
--- Scripts/seeds.gd
+++ Scripts/seeds.gd
@@ -2,22 +2,25 @@ extends "item.gd"
 
 @export var plant_scene: PackedScene
 @export var count: int = 3
-@export var bool_plant: int = 1;
 
 @onready var text: RichTextLabel = $RichTextLabel
 @onready var sprite: Sprite2D = $Sprite2D
 
+func _custom_ready() -> void:
+	text.text = format(count)
+
 func _custom_process(_delta):
-	if picked_up && Input.is_action_just_released("interact") && bool_plant == 1:
-		if (count > 0):
-			count -= 1
-			if (count == 0):
-				text.queue_free()
-				sprite.queue_free();
-				bool_plant = 0;
-			else:
-				text.text = str(count)
+	if picked_up && Input.is_action_just_released("interact"):
+		count -= 1
+
 		var plant: Node2D = plant_scene.instantiate()
-		#neal write code to snap to tile
 		plant.position = player.position
 		get_tree().root.add_child(plant)
+
+		text.text = format(count)
+
+		if count == 0:
+			queue_free()
+
+func format(x: int) -> String:
+	return "[center]" + str(x) + "[/center]"
