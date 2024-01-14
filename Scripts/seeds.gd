acce77d7765e3da8444596e79736633b714fcd58
Neal Wang <nealwang.sh@protonmail.com>
2024-01-13 23:18:25 -0800

    yes

--- Scripts/seeds.gd
+++ Scripts/seeds.gd
@@ -3,7 +3,7 @@ extends "item.gd"
 @export var plant_scene: PackedScene
 @export var count: int = 3
 @export var plant_offset: int = 32
-@export_flags_2d_physics var mask: int
+@export_flags_2d_physics var mask: int = 4
 
 @onready var text: RichTextLabel = $RichTextLabel
 @onready var sprite: Sprite2D = $Sprite2D
