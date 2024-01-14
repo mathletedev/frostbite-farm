1373a69d27ee4a4b600d342057df60d65cfa0bee
CharlesLiuCool <charles.boyan.liu@gmail.com>
2024-01-13 21:26:45 -0800

    fixed seed snapping

--- Scripts/seeds.gd
+++ Scripts/seeds.gd
@@ -2,6 +2,7 @@ extends "item.gd"
 
 @export var plant_scene: PackedScene
 @export var count: int = 3
+@export var plant_offset: int = 32
 
 @onready var text: RichTextLabel = $RichTextLabel
 @onready var sprite: Sprite2D = $Sprite2D
@@ -15,13 +16,13 @@ func _custom_process(_delta):
 
 		var plant: Node2D = plant_scene.instantiate()
 		plant.position.x = (int(player.position.x) / 32) * 32 + 16
-		plant.position.y = (int(player.position.y) / 32) * 32 + 16
+		plant.position.y = (int(player.position.y + plant_offset) / 32) * 32 - 16
 		get_tree().root.add_child(plant)
 
 		text.text = format(count)
 
 		#if count == 0:
-			#queue_free()
+		#	queue_free()
 
 func format(x: int) -> String:
 	return "[center]" + str(x) + "[/center]"
