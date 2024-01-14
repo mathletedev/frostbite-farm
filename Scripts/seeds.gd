738788cae5fe9c9dc59605e28b243d7edf8562f4
Neal Wang <nealwang.sh@protonmail.com>
2024-01-13 23:04:40 -0800

    yes

--- Scripts/seeds.gd
+++ Scripts/seeds.gd
@@ -3,6 +3,7 @@ extends "item.gd"
 @export var plant_scene: PackedScene
 @export var count: int = 3
 @export var plant_offset: int = 32
+@export_flags_2d_physics var mask: int
 
 @onready var text: RichTextLabel = $RichTextLabel
 @onready var sprite: Sprite2D = $Sprite2D
@@ -18,7 +19,16 @@ func _custom_process(_delta):
 		point.x = (float(player.position.x / 32)) * 32 + 16
 		point.y = (float((player.position.y + plant_offset) / 32)) * 32 - 16
 
-		var collisions = get_world_2d().direct_space_state.intersect_point()
+		var parameters := PhysicsPointQueryParameters2D.new()
+		parameters.position = point
+		parameters.collide_with_areas = true
+		parameters.collide_with_bodies = false
+		parameters.collision_mask = mask
+
+		var collisions := get_world_2d().direct_space_state.intersect_point(parameters)
+		print(collisions)
+		if collisions.size() > 0:
+			return
 
 		var plant: Node2D = plant_scene.instantiate()
 		plant.position = point
