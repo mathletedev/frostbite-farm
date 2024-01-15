fe125e0c068cd418a8ae0ed01c96a59b6b490f2b
Neal Wang <nealwang.sh@protonmail.com>
2024-01-14 19:52:40 -0800

    game complete :moai:

--- Scenes/escape_menu.gd
+++ Scenes/escape_menu.gd
@@ -3,10 +3,17 @@ extends Node
 @onready var player_ui: CanvasLayer = get_node("UI")
 @onready var escape_menu: CanvasLayer = get_node("EscapeMenu")
 
-func _process(delta):
+func _ready() -> void:
+	player_ui.visible = false
+
+	get_tree().paused = true
+
+func _process(_delta):
 	if Input.is_action_just_pressed("escape_menu"):
 		player_ui.visible = false
 		escape_menu.visible = true
+
+		get_tree().paused = true
 
 func _on_exit_button_pressed():
 	get_tree().quit()
@@ -15,3 +22,4 @@ func _on_resume_button_pressed():
 	player_ui.visible = true
 	escape_menu.visible = false
 
+	get_tree().paused = false
--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -3,9 +3,12 @@ extends Camera2D
 @export var tween_speed = 3
 
 @onready var player: Node2D = get_node("%Player")
+# @onready var particles: GPUParticles2D = $GPUParticles2D
 
 func _ready() -> void:
 	position = player.position
+
+	# particles.position.y = -(get_viewport_rect().size * get_viewport().get_canvas_transform().affine_inverse()).y / 2.0
 
 func _physics_process(delta: float) -> void:
 	position += (player.position - position) * tween_speed * delta
